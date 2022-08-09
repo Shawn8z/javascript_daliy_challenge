8b09a82517a0f086e1f1895e106f59a7cd5f39c3
Shawn <lcorel86@gmail.com>
2022-08-08 21:35:21 -0500

    promise then

---
+++ daliy_challenge_Promises_V_Then.js
@@ -0,0 +1,38 @@
+// const promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("Bringing tacos");
+//     }, 5000)
+// })
+
+// const promise2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("Not bringing tacos, foot truck not there.");
+//     }, 5000)
+// })
+
+// const onFulfillment = (result) => {
+//     console.log(result);
+//     console.log("set up the table to eat tacos");
+// }
+
+// const onRejection = (error) => {
+//     console.log(error);
+//     console.log('start cooking pasta');
+// }
+
+// promise1.then(onFulfillment);
+// promise2.catch(onRejection);
+
+
+let result = "";
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        result = "success"
+        resolve(result);
+    }, 1000)
+})
+
+
+
+promise.then((val) =>console.log(val));
