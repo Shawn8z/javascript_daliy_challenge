630b74b6c2d7ceb02e1a5f304305d0f8cd9622fb
Shawn <lcorel86@gmail.com>
2022-08-10 19:23:28 -0500

    promise old composition introduction

---
+++ daliy_challenge_Promises_IX_Old_Composition_Introduction.js
@@ -0,0 +1,34 @@
+// const promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("Bringing tacos");
+//     }, 5000)
+// })
+
+// const promise2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("Not bringing tacos, foot truck not there.");
+//     }, 5000)
+// })
+
+// const onFulfillment = (result) => {
+//     console.log(result);
+//     console.log("set up the table to eat tacos");
+// }
+
+// const onRejection = (error) => {
+//     console.log(error);
+//     console.log('start cooking pasta');
+// }
+
+// promise1.then(onFulfillment);
+// promise2.catch(onRejection);
+
+
+var result = ""
+var thenable = { then: function(onFulfill, onReject) { onFulfill("edabit!") }}
+var promise = Promise.resolve(thenable);
+promise.then((value) => {console.log(value)});
+
+
+
+
