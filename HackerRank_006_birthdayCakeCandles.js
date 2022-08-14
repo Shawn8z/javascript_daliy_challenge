490a8caf6f420d39410e1cc4d0e6f7f2b1515386
Shawn <lcorel86@gmail.com>
2022-08-13 21:29:16 -0500

    starting timeConversion

--- HackerRank_006_birthdayCakeCandles.js
+++ HackerRank_006_birthdayCakeCandles.js
@@ -1,7 +1,21 @@
 function birthdayCakeCandles(candles) {
     // Write your code here
     let maxNum = Math.max(...candles);
-    console.log(maxNum);
+    let maxNumCount = 0;
+    let candlesCount = candles.length;
+
+    for (let i = 0; i < candles.length; i++) {
+        if (candles[i] == maxNum) {
+            maxNumCount += 1;
+        }
+    }
+
+    return maxNumCount;
 }
+
+
+let val = [3, 2, 1, 3];
+let result = birthdayCakeCandles(val);
+console.log(result);
 
 
