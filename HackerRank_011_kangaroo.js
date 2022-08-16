d21f7a9724ae9dac0e3540a00822f7ab8fbdf237
Shawn <lcorel86@gmail.com>
2022-08-16 01:35:43 -0500

    kangaroo is done

--- HackerRank_011_kangaroo.js
+++ HackerRank_011_kangaroo.js
@@ -1,44 +1,21 @@
-function countApplesAndOranges(s, t, a, b, apples, oranges) {
+function kangaroo(x1, v1, x2, v2) {
     // Write your code here
-    let houseStart = s;
-    let houseEnd = t;
 
-    let appleTreePos = a;
-    let orangeTreePos = b;
-
-    let applePosArr = [];
-    let appleLength = apples.length;
-
-    for (let i = 0; i < appleLength; i++) {
-        let applePos = appleTreePos + apples[i];
-        applePosArr.push(applePos);
-    } 
-
-    let orangePosArr = [];
-    let orangeLength = oranges.length;
-
-    for (let i = 0; i < orangeLength; i++) {
-        let orangePos = orangeTreePos + oranges[i];
-        orangePosArr.push(orangePos);
+    if ((x2 > x1 && v2 > v1)) {
+        return "NO";
     }
 
-    let appleCount = 0;
-    for (let i = 0; i < appleLength; i++) {
-        if (applePosArr[i] >= houseStart && applePosArr[i] <= houseEnd) {
-            appleCount += 1;
-        }
+    let steps = (x2 - x1) / (v1 - v2);
+    
+    if (Number.isInteger(steps)) {
+        return "YES";
+    } else {
+        return "NO"
     }
-
-    let orangeCount = 0;
-    for (let i = 0; i < orangeLength; i++) {
-        if (orangePosArr[i] >= houseStart && orangePosArr[i] <= houseEnd) {
-            orangeCount += 1;
-        }
-    }
-
-    console.log(appleCount);
-    console.log(orangeCount);
 }
 
 
-countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
+let val = kangaroo(14, 4, 98, 2);
+
+
+console.log(val);
