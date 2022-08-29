6220bf0a5cd77f10997392607b560aa5d96736ae
Shawn <lcorel86@gmail.com>
2022-08-28 23:50:33 -0500

    form magic square done

--- HackerRank_024_formingMagicSquare.js
+++ HackerRank_024_formingMagicSquare.js
@@ -3,33 +3,8 @@ function formingMagicSquare(s) {
     // for a 3 x 3 magic square, its line sum depend on the center value, all line sum should equals
     // 3 * center value, so you find what every line sum is and add all the differences together
 
-    // let lineSum = s[1][1] * 3;
-    // let lineSum = 15;
-    // let result = 0;
-    
 
-
-    // let rightDiagonal = [s[0][0], s[1][1], s[2][2]];
-    // let leftDiagonal = [s[0][2], s[1][1], s[2][0]];
-    // let centerColumn = [s[0][1], s[1][1], s[2][1]];
-    // let centerRow = [s[1][0], s[1][1], s[1][2]];
-    // let allLines = [rightDiagonal, leftDiagonal, centerColumn, centerRow]
-
-    // for (let item of allLines) {
-    //     result += lineSumDifference(item);
-    // }
-    
-    // function lineSumDifference(roll) {
-    //     let rollSum = roll.reduce((pVal, cVal) => pVal + cVal, 0);
-
-    //     return lineSum - rollSum !== 0 ? Math.abs(lineSum - rollSum) : 0;
-    // }
-    
-    
-    // return result;
-
-
-    const possibleMagicSquares = [
+    const allCombos = [
         [2, 9, 4, 7, 5, 3, 6, 1, 8],
         [2, 7, 6, 9, 5, 1, 4, 3, 8],
         [4, 3, 8, 9, 5, 1, 2, 7, 6],
@@ -40,17 +15,21 @@ function formingMagicSquare(s) {
         [6, 1, 8, 7, 5, 3, 2, 9, 4]
     ] 
         
-    const s_flat = s.flat();
-    let totalDifferent = [];
-    for(let i = 0; i < possibleMagicSquares.length; i ++) {
+    const arr1D = s.flat();
+    let totalDifference = [];
+    
+    for (let array of allCombos) {
         let total = 0;
-        s_flat.reduce((previous, current, index) => {
-            total = previous + Math.abs(current - possibleMagicSquares[i][index]);
-            return total
-        }, 0)
-        totalDifferent.push(total);
+
+        for (let i = 0; i < 9; i++) {
+            total += Math.abs(array[i] - arr1D[i])
+        }
+
+        totalDifference.push(total);
     }
-    return Math.min(...totalDifferent);
+
+    return Math.min(...totalDifference);
+    
 }
 
 
@@ -79,6 +58,11 @@ let s4 = [
             [6, 1, 6]
         ]
 
+let s5 = [
+            [2, 5, 4],
+            [4, 6, 9],
+            [4, 5, 2]
+        ]
 
-let val = formingMagicSquare(s4);
+let val = formingMagicSquare(s5);
 console.log(val);
