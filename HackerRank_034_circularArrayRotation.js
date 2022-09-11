aa82752b90bfff0f4e628984a18263e5e590b9cf
Shawn <lcorel86@gmail.com>
2022-09-10 21:06:11 -0500

    more about modulos

---
+++ HackerRank_034_circularArrayRotation.js
@@ -0,0 +1,38 @@
+function circularArrayRotation(a, k, queries) {
+    // Write your code here
+    let arr = a;
+    let steps = k;
+
+    let tempArr = [...arr]
+
+    // for (let i = 0; i < steps; i++) {
+    //     tempArr = shiftLastArrItemToFront(tempArr);
+    // }
+
+    for (let i = 0; i < a.length; i++) {
+        
+    }
+    
+
+    // function shiftLastArrItemToFront(arr) {
+    //     let lastItem = arr.slice(-1);
+    //     let remainingArr = arr.slice(0, -1);
+
+    //     return [...lastItem, ...remainingArr];
+    // }
+    
+    // let resultArr = [];
+    // for (let index in queries) {
+    //     resultArr.push(tempArr[index]);
+    // }
+
+    // return resultArr;
+}
+
+
+let routationCount = 2;
+let numArr = [1, 2, 3];
+let queries = [0, 1, 2];
+
+
+circularArrayRotation(numArr, routationCount, queries); 
--- randomchanglleng.js
+++ randomchanglleng.js
@@ -1,19 +1,4 @@
-function arrZip1(ar1, ar2) {
-    let resultArr = [];
+function circularArrayRotation(a, k, queries) {
+    // Write your code here
 
-    for (let i = 0; i < 3; i++) {
-        resultArr.push(ar1[i]);
-
-        resultArr.push(ar2[i]);
-    }
-
-    console.log(resultArr);
 }
-
-
-
-let arr1 = [1, 3, 5];
-let arr2 = [2, 4, 6];
-
-// arrZip1(arr1, arr2);
-arrZip2(arr1, arr2);
