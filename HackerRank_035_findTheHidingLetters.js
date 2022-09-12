d7223d7aeb23e9f411164eb27a098d700caaef52
Shawn <lcorel86@gmail.com>
2022-09-12 00:46:09 -0500

    hidding letters done

---
+++ HackerRank_035_findTheHidingLetters.js
@@ -0,0 +1,16 @@
+function findLetters(str) {
+    let regex  = /[a-z]/g;
+    let result = str.match(regex).join("");
+    console.log(result);
+    return result;
+}
+
+
+
+
+let test1 = "UcUNFYGaFYFYGtNUH";
+let test2 = "bEEFGBuFBRrHgUHlNFYaYr";
+let test3 = "YFemHUFBbezFBYzFBYLleGBYEFGBMENTment";
+
+
+findLetters(test3);
