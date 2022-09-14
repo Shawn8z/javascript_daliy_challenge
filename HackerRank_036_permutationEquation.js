31b36fc2742e6daadb867103f282a89e65d1a3fb
Shawn <lcorel86@gmail.com>
2022-09-13 21:25:09 -0500

    permutationEuqation done

--- HackerRank_036_permutationEquation.js
+++ HackerRank_036_permutationEquation.js
@@ -2,27 +2,33 @@ function permutationEquation(p) {
     // Write your code here
     let arrLength = p.length;
     let testNum = [];
+    let indexArr = [];
     let resultArr = [];
 
     for (let i = 1; i <= arrLength; i++) {
         testNum.push(i);
     }
-    console.log(testNum);
+    
 
     for (let i = 0; i < arrLength; i++) {
         let index = p.findIndex((item) => item === testNum[i]);
-        resultArr.push(index);
+        indexArr.push(index + 1);
+    }
+
+
+    for (let i = 0; i < arrLength; i++) {
+        let index = p.findIndex((item) => item === indexArr[i]);
+        resultArr.push(index + 1);
     }
 
     console.log(resultArr);
 
-
-
+    return resultArr;
 
 }
 
 
 
-let arr = [4, 3, 5, 1, 2];
-
-permutationEquation(arr);
+let arr1 = [4, 3, 5, 1, 2];
+let arr2 = [5, 2, 1, 3, 4];
+permutationEquation(arr2);
