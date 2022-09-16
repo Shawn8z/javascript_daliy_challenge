b084982b74de408000cf27b3803fb118d35fe949
Shawn <lcorel86@gmail.com>
2022-09-15 23:15:29 -0500

    findDigits done

--- HackerRank_039_findDigits.js
+++ HackerRank_039_findDigits.js
@@ -1,4 +1,23 @@
 function findDigits(n) {
     // Write your code here
+    let numStr = n + "";
+    let strArr = numStr.split("");
+    let numArr = strArr.map((item) => Number(item));
+    let suitableItem = [];
 
+    for (let i = 0; i < numArr.length; i++) {
+        
+        if (numArr[i] == 0) {
+            continue;
+        }
+
+        if (n % numArr[i] == 0) {
+            suitableItem.push(numArr[i])
+        }
+    }
+
+    return suitableItem.length;
 }
+
+let val = findDigits(1012);
+console.log(val);
