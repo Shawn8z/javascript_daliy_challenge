ed11a9cc5079e18440d618a636035227fc228cf7
Shawn <lcorel86@gmail.com>
2022-10-20 20:16:42 -0500

    fair Rations done

---
+++ HackerRank_065_fairRations.js
@@ -0,0 +1,35 @@
+function fairRations(B) {
+    // Write your code here
+    let breadArr = B;
+    let length = breadArr.length;
+    let result = 0;
+
+
+    for (let i = 0; i < length; i++) {
+        if ( !isItemEven(i) && i != length - 1 ) {
+            breadArr[i] += 1;
+            breadArr[i + 1] += 1;
+            result += 2;
+        }
+    }
+
+    if ( isItemEven(length - 1) ) {
+        return String(result);
+    } else {
+        return "NO";
+    }
+
+    function isItemEven(index) {
+        if (breadArr[index] % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+let arr = [1, 2];
+
+let val = fairRations(arr);
+console.log(val);
