6888f998f8fc7c5e7d8343809da468f95d26db9b
Shawn <lcorel86@gmail.com>
2022-10-28 01:16:19 -0500

    manasaAndStonds

---
+++ HackerRank_069_manasaAndStones.js
@@ -0,0 +1,22 @@
+function stones(n, a, b) {
+    // Write your code here
+    let stoneCount = n;
+    let stoneVal = 0;
+
+    let diff1 = a;
+    let diff2 = b;
+
+    let resultArr = [];
+    let diffCombos = [[diff1, diff1], [diff1, diff2], [diff2, diff1], [diff2, diff2]];
+    
+    for (let i = stoneVal; i < stoneCount-1; i++) {
+
+    }
+
+    function
+}
+
+
+let arr1 = [3, 1, 2];
+
+stones(...arr1);
