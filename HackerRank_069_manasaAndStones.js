4221fda3d77b8b90f6f56b2d9df7c838d29e1f22
Shawn <lcorel86@gmail.com>
2022-10-29 21:48:42 -0500

    need to add diffCombobuiler

--- HackerRank_069_manasaAndStones.js
+++ HackerRank_069_manasaAndStones.js
@@ -9,14 +9,42 @@ function stones(n, a, b) {
     let resultArr = [];
     let diffCombos = [[diff1, diff1], [diff1, diff2], [diff2, diff1], [diff2, diff2]];
     
-    for (let i = stoneVal; i < stoneCount-1; i++) {
+    
 
+    for (let i = 0; i < diffCombos.length; i++) {
+        let diffs = diffCombos[i];
+
+        let combo = comboMaker(n, diffs);
+        console.log(combo);
+        // resultArr.push(combo);
     }
 
-    function
+    function comboMaker(n, diffCombos, index) {
+        let diffArr = diffCombos;
+        let rockCount = n;
+        let result = [0];
+
+        for (let i = 1; i < rockCount; i++) {
+            let index = (i-1) % 2;
+            // console.log(`the i is ${i}, the index is ${index}`);
+
+            let val = result[i - 1] + diffArr[index];
+            result.push(val);
+            // console.log(result);
+            
+        }
+
+        // console.log(result);
+        return result;
+    }
+
+    function diffComboBuilder() {
+        
+    }
 }
 
 
 let arr1 = [3, 1, 2];
+let arr2 = [4, 10, 100];
 
-stones(...arr1);
+stones(...arr2);
